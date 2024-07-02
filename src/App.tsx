c9e7e97d41f3731e5b177dd0cff4f119f9660f62
Alexz Desktop <chikofarrel@example.com>
2024-07-02 22:53:40 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import IntroViews from "./templates/views/introViews";
+
+function App() {
+  return (
+    <React.Fragment>
+      <IntroViews />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/templates/views/introViews.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Hasby from "../../assets/hasby";
+import Hasby2 from "../../assets/hasby2";
+
+export default function IntroViews() {
+  return (
+    <React.Fragment>
+      <div className="w-full h-screen grid place-items-center">
+        <div className="max-w-xl p-5 h-screen flex flex-col justify-center bg-gray-200/80">
+          <Hasby />
+          <Hasby2 />
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
